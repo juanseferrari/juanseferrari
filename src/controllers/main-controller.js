0a40e09c1a8fc1871b87044810e656f21e2cf9a1
juanseferrari <juansegundoferrari@gmail.com>
2023-12-28 15:29:32 -0300

    agendar

--- src/controllers/main-controller.js
+++ src/controllers/main-controller.js
@@ -40,7 +40,7 @@ const mainController = {
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta property="og:title" content="Juanse Ferrari">
+        <meta property="og:title" content="Juanse Ferrari - Linkedin">
         <meta property="og:description" content="Programador, entusiasta y fanático del mundo Fintech.">
         <meta property="og:image" content="https://www.juanseferrari.com/images/juanse2.png">
 
@@ -61,7 +61,34 @@ const mainController = {
     `);
   },
   agenda: (req,res) => {
-    res.redirect("https://calendar.app.google/Gpq2QUcsDSvuMVmd6")
+
+     // Send HTML response with Open Graph meta tags
+     res.send(`
+     <!DOCTYPE html>
+     <html lang="en">
+     <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <meta property="og:title" content="Juanse Ferrari - Agendar reunión ">
+       <meta property="og:description" content="Programador, entusiasta y fanático del mundo Fintech.">
+       <meta property="og:image" content="https://www.juanseferrari.com/images/juanse2.png">
+
+       <!-- Add more meta tags as needed -->
+       <title>Juanse Ferrari</title>
+     </head>
+     <body>
+       <!-- Your HTML content goes here -->
+
+       <script>
+         // Redirect to the desired URL after a delay (e.g., 3 seconds)
+         setTimeout(function() {
+           window.location.href = 'https://calendar.app.google/Gpq2QUcsDSvuMVmd6';
+         }, 300);
+       </script>
+     </body>
+     </html>
+   `);
+
   }
 };
 
