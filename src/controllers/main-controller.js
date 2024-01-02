8605f574cc1e8e43d28ecd1376903a2df43984bb
juanseferrari <juansegundoferrari@gmail.com>
2024-01-02 16:58:24 -0300

    cambio nombre

--- src/controllers/main-controller.js
+++ src/controllers/main-controller.js
@@ -40,7 +40,7 @@ const mainController = {
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta property="og:title" content="Juanse Ferrari - Linkedin">
+        <meta property="og:title" content="Juanse Ferrari | Linkedin">
         <meta property="og:description" content="Programador, entusiasta y fanático del mundo Fintech.">
         <meta property="og:image" content="https://www.juanseferrari.com/images/juanse2.png">
 
@@ -69,7 +69,7 @@ const mainController = {
      <head>
        <meta charset="UTF-8">
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <meta property="og:title" content="Juanse Ferrari - Agendar reunión ">
+       <meta property="og:title" content="Juanse Ferrari | Agendar reunión ">
        <meta property="og:description" content="Programador, entusiasta y fanático del mundo Fintech.">
        <meta property="og:image" content="https://www.juanseferrari.com/images/juanse2.png">
 
